cf13a7da36680d9eebcd42f551630e7b5e8ba49b
Ryad-Elhalawany <ryad.elhalawany@gmail.com>
2026-05-07 03:13:38 +0300

    token Fix

--- src/app/myUtil.ts
+++ src/app/myUtil.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export async function getUserToken() {
     const myCookies = await cookies()
-    const sessionToken = myCookies.get('next-auth.session-token')?.value;
+    const sessionToken = myCookies.get('__Secure-next-auth.session-token')?.value;
 
     const token = await decode({ token: sessionToken, secret: process.env.NEXTAUTH_SECRET || "" })
 
